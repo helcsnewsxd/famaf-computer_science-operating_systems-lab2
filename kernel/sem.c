3617ec402cee18df0a2f127fadceecfd6f18a508
Lautaro Bachmann <laurigbach@gmail.com>
2022-09-27 19:21:06 -0300

    Añadir definicion de prueba de las syscalls del semaforo

---
+++ kernel/sem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int sys_sem_open(int sem, int value){
+  printf("Soy sys_sem_open");
+  return 0;
+}
+
+int sys_sem_up(int sem){
+  printf("Soy sys_sem_up");
+  return 0;
+}
+
+int sys_sem_close(int sem){
+  printf("Soy sys_sem_close");
+  return 0;
+}
+
+int sys_sem_down(int sem){
+  printf("Soy sys_sem_down");
+  return 0;
+}
