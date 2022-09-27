fe92e78910a43e501bef55f97c77bd6f952748b8
Lautaro Bachmann <laurigbach@gmail.com>
2022-09-27 19:18:28 -0300

    Declarar syscalls del semaforo en modulo syscall

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -101,6 +101,10 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
+extern uint64 sys_sem_open(int sem, int value);
+extern uint64 sys_sem_up(int sem);
+extern uint64 sys_sem_close(int sem);
+extern uint64 sys_sem_down(int sem);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -126,6 +130,10 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_sem_open] sys_sem_open,
+[SYS_sem_close] sys_sem_close,
+[SYS_sem_up] sys_sem_up,
+[SYS_sem_down] sys_sem_down,
 };
 
 void
