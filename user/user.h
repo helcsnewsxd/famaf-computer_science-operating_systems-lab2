06c01595aac190de48140ab6bfe155204ffe43c1
Juanbratti <juanbratti@mi.unc.edu.ar>
2022-09-27 21:51:11 +0000

    Adición de sem_open, sem_close, sem_up y sem_down al archivo user.h

--- user/user.h
+++ user/user.h
@@ -22,6 +22,10 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int sem_open(int, int);
+int sem_close(int);
+int sem_up(int);
+int sem_down(int);
 
 // ulib.c
 int stat(const char*, struct stat*);
