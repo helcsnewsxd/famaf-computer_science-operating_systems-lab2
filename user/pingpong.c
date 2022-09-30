01cd0cfcce44f158251c2fb8b57bf15ade95a16d
EmanuelHerrador <emanuel.nicolas.herrador@unc.edu.ar>
2022-09-30 13:38:11 -0300

    Hecho todo el programa de ping pong para que cumpla con lo pedido con el enunciado usando threads (ya que forkeo) Habiendo arreglado las syscalls antes, anda joya

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,12 +2,57 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+#define exit_error(error) if(error == 0){printf("Error in pingpong\n");return 0;}
+
 int
 main(int argc, char *argv[])
 {
-  sem_open(1,0);
-  sem_up(1);
-  sem_down(1);
-  sem_close(1);
-  return 0;
+  if(argc != 2){
+    exit_error(0);
+
+  }
+
+  int error = 1;
+
+  error = sem_open(0,1); // Ping
+  exit_error(error);
+  error = sem_open(1,0); // Pong
+  exit_error(error);
+
+  int N = atoi(argv[1]);
+  
+  int pid = fork();
+  if(pid < 0){
+    exit_error(0);
+
+  }else if(pid == 0){
+    for(unsigned int i = 0; i < N; i++){
+      error = sem_down(0);
+      exit_error(error);
+      
+      printf("ping\n");
+
+      error = sem_up(1);
+      exit_error(error);
+    }
+
+  }else{
+    for(unsigned int i = 0; i < N; i++){
+      error = sem_down(1);
+      exit_error(error);
+
+      printf("pong\n");
+
+      error = sem_up(0);
+      exit_error(error);
+    }
+
+    error = sem_close(0);
+    exit_error(error);
+
+    error = sem_close(1);
+    exit_error(error);
+  }
+
+  return 1;
 }
