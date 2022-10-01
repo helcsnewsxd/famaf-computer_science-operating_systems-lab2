cb3fa7e4e1e704c3bc6d797b7a5a855433466dca
Juanbratti <juanbratti@mi.unc.edu.ar>
2022-10-01 20:04:07 +0000

    Agregados comentarios al programa pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -14,18 +14,20 @@ main(int argc, char *argv[])
 
   int error = 1;
 
-  error = sem_open(0,1); // Ping
+  // abrimos un semáforo para el ping
+  error = sem_open(0,1);
   exit_error(error);
-  error = sem_open(1,0); // Pong
+  // abrimos un semáforo para el pong
+  error = sem_open(1,0); 
   exit_error(error);
 
   int N = atoi(argv[1]);
   
-  int pid = fork();
+  int pid = fork(); // Hacemos el ping en el proceso hijo y el pong en el proceso padre.
   if(pid < 0){
     exit_error(0);
 
-  }else if(pid == 0){
+  }else if(pid == 0){ // proceso hijo
     for(unsigned int i = 0; i < N; i++){
       error = sem_down(0);
       exit_error(error);
@@ -36,7 +38,7 @@ main(int argc, char *argv[])
       exit_error(error);
     }
 
-  }else{
+  }else{ // proceso padre
     for(unsigned int i = 0; i < N; i++){
       error = sem_down(1);
       exit_error(error);
@@ -47,9 +49,10 @@ main(int argc, char *argv[])
       exit_error(error);
     }
 
-    error = sem_close(0);
+    // Cerramos semáforo
+    error = sem_close(0); 
     exit_error(error);
-
+    // Cerramos semáforo
     error = sem_close(1);
     exit_error(error);
   }
