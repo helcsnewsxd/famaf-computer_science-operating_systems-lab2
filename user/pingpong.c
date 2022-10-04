9dcd6a9016aed80b81d2611de32413afddb474ac
Juanbratti <juanbratti@mi.unc.edu.ar>
2022-10-04 01:40:52 +0000

    comentarios a inglés

--- user/pingpong.c
+++ user/pingpong.c
@@ -13,20 +13,20 @@ main(int argc, char *argv[])
 
   int error = 1;
 
-  // abrimos un semáforo para el ping
+  // Opening a semaphore for "ping"
   error = sem_open(0,1);
   exit_if_zero(error,"Error con open semáforos");
-  // abrimos un semáforo para el pong
+  // Opening a semaphore for "pong"
   error = sem_open(1,0); 
   exit_if_zero(error,"Error con open semáforos");
 
   int N = atoi(argv[1]);
   
-  int pid = fork(); // Hacemos el ping en el proceso hijo y el pong en el proceso padre.
+  int pid = fork(); // Ping prints in the child process, pong in the parent process
   if(pid < 0){
     exit_if_zero(0,"Error con fork");
 
-  }else if(pid == 0){ // proceso hijo
+  }else if(pid == 0){ // child process
     for(unsigned int i = 0; i < N; i++){
       error = sem_down(0);
       exit_if_zero(error,"Error con down semáforos");
@@ -37,7 +37,7 @@ main(int argc, char *argv[])
       exit_if_zero(error,"Error con up semáforos");
     }
 
-  }else{ // proceso padre
+  }else{ // parent process
     for(unsigned int i = 0; i < N; i++){
       error = sem_down(1);
       exit_if_zero(error,"Error con down semáforos");
@@ -48,10 +48,10 @@ main(int argc, char *argv[])
       exit_if_zero(error,"Error con up semáforos");
     }
 
-    // Cerramos semáforo
+    // Closing the semaphore
     error = sem_close(0); 
     exit_if_zero(error,"Error con close semáforos");
-    // Cerramos semáforo
+    // Closing the semaphore
     error = sem_close(1);
     exit_if_zero(error,"Error con close semáforos");
   }
